a34034b3deaafa1893c3ed25b805215063563103
alvinKyle <alvinkyle70@gmail.com>
2024-03-24 15:41:43 +0300

    update lab

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Write your solution here!
+
+const cats =["Milo", "Otis","Garfield" ];
+ 
+function destructivelyAppendCat(name) {
+  cats.push("Ralph");
+}
+function destructivelyPrependCat(name) {
+    cats.unshift("Bob");
+}
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+function appendCat() {
+    const cats = ["Milo", "Otis","Garfield" ];
+    const newCats = [...cats ,"Broom"];
+    return newCats;
+}
+function prependCat() {
+    const cats = ["Milo", "Otis","Garfield" ];
+    const moreCats = [ "Arnold",...cats]
+    return moreCats; 
+}
+function removeLastCat() {
+    const cats = ["Milo", "Otis","Garfield" ];
+    const removeCat =cats.slice(cats,cats.length - 1);
+    return removeCat;
+}
+function removeFirstCat() {
+    const cats = ["Milo", "Otis","Garfield" ];
+    const removeCat1 = cats.splice(1)
+    return removeCat1;
+    
+}
+
+
+
+
+
+ 
